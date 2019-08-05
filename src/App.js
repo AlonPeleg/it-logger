9d550f320f26dd25730cdc0c7d74ceeef3e6769d
AlonPeleg <peleg619@gmail.com>
2019-08-05 21:07:31 +0300

    added Edit log modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import Addbtn from "./components/layout/Addbtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -20,6 +21,7 @@ const App = () => {
       <div className="container">
         <Addbtn />
         <AddLogModal />
+        <EditLogModal />
         <Logs />
       </div>
     </Fragment>
